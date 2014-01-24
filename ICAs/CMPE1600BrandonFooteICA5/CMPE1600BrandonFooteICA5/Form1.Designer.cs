168c766270bf911eb017584a67e6a9c20b2c9fd6
Brandon Foote <brandon.foote@gmail.com>
2014-01-24 09:26:22 -0700

    ICA 5 Complete
    Form Layout
    - Progress Bars:
    - Added Number Progress bar
    - Added User Input Progress bar
    - Added Form Reset Progress bar
    Code:
    - Added counter to handle timer for userinput
    - Added timer to handle Form Reset
    - Added code to increment Progress Bars
    - Added code to limit user input to 3 seconds
    - Added reset form after 5 seconds from results display
    - Focus changes to user input box upon final number display

--- ICAs/CMPE1600BrandonFooteICA5/CMPE1600BrandonFooteICA5/Form1.Designer.cs
+++ ICAs/CMPE1600BrandonFooteICA5/CMPE1600BrandonFooteICA5/Form1.Designer.cs
@@ -36,11 +36,14 @@
             this.lblResultsDisplay = new System.Windows.Forms.Label();
             this.tmrGame = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pgbNumber = new System.Windows.Forms.ProgressBar();
+            this.pgbInput = new System.Windows.Forms.ProgressBar();
+            this.pgbClear = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // btnStartButton
             // 
-            this.btnStartButton.Location = new System.Drawing.Point(143, 243);
+            this.btnStartButton.Location = new System.Drawing.Point(162, 243);
             this.btnStartButton.Name = "btnStartButton";
             this.btnStartButton.Size = new System.Drawing.Size(88, 33);
             this.btnStartButton.TabIndex = 0;
@@ -50,7 +53,8 @@
             // 
             // tbxUserInput
             // 
-            this.tbxUserInput.Location = new System.Drawing.Point(127, 207);
+            this.tbxUserInput.Enabled = false;
+            this.tbxUserInput.Location = new System.Drawing.Point(146, 207);
             this.tbxUserInput.Name = "tbxUserInput";
             this.tbxUserInput.Size = new System.Drawing.Size(121, 20);
             this.tbxUserInput.TabIndex = 1;
@@ -59,7 +63,7 @@
             // 
             this.lblNumDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblNumDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumDisplay.Location = new System.Drawing.Point(124, 38);
+            this.lblNumDisplay.Location = new System.Drawing.Point(143, 38);
             this.lblNumDisplay.Name = "lblNumDisplay";
             this.lblNumDisplay.Size = new System.Drawing.Size(127, 135);
             this.lblNumDisplay.TabIndex = 2;
@@ -68,20 +72,20 @@
             // lblResults
             // 
             this.lblResults.AutoSize = true;
-            this.lblResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblResults.Location = new System.Drawing.Point(36, 301);
+            this.lblResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResults.Location = new System.Drawing.Point(12, 301);
             this.lblResults.Name = "lblResults";
-            this.lblResults.Size = new System.Drawing.Size(67, 20);
+            this.lblResults.Size = new System.Drawing.Size(56, 16);
             this.lblResults.TabIndex = 3;
             this.lblResults.Text = "Results:";
             // 
             // lblResultsDisplay
             // 
             this.lblResultsDisplay.AutoSize = true;
-            this.lblResultsDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblResultsDisplay.Location = new System.Drawing.Point(112, 301);
+            this.lblResultsDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultsDisplay.Location = new System.Drawing.Point(74, 301);
             this.lblResultsDisplay.Name = "lblResultsDisplay";
-            this.lblResultsDisplay.Size = new System.Drawing.Size(62, 20);
+            this.lblResultsDisplay.Size = new System.Drawing.Size(55, 16);
             this.lblResultsDisplay.TabIndex = 4;
             this.lblResultsDisplay.Text = "Tracker";
             this.lblResultsDisplay.Visible = false;
@@ -91,11 +95,38 @@
             // 
             this.tmrGame.Tick += new System.EventHandler(this.tmrGame_Tick);
             // 
+            // pgbNumber
+            // 
+            this.pgbNumber.Location = new System.Drawing.Point(15, 38);
+            this.pgbNumber.Maximum = 3;
+            this.pgbNumber.Name = "pgbNumber";
+            this.pgbNumber.Size = new System.Drawing.Size(100, 23);
+            this.pgbNumber.TabIndex = 5;
+            // 
+            // pgbInput
+            // 
+            this.pgbInput.Location = new System.Drawing.Point(15, 67);
+            this.pgbInput.Maximum = 6;
+            this.pgbInput.Name = "pgbInput";
+            this.pgbInput.Size = new System.Drawing.Size(100, 23);
+            this.pgbInput.TabIndex = 6;
+            // 
+            // pgbClear
+            // 
+            this.pgbClear.Location = new System.Drawing.Point(15, 96);
+            this.pgbClear.Maximum = 10;
+            this.pgbClear.Name = "pgbClear";
+            this.pgbClear.Size = new System.Drawing.Size(100, 23);
+            this.pgbClear.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 355);
+            this.ClientSize = new System.Drawing.Size(412, 355);
+            this.Controls.Add(this.pgbClear);
+            this.Controls.Add(this.pgbInput);
+            this.Controls.Add(this.pgbNumber);
             this.Controls.Add(this.lblResultsDisplay);
             this.Controls.Add(this.lblResults);
             this.Controls.Add(this.lblNumDisplay);
@@ -118,6 +149,9 @@
         private System.Windows.Forms.Label lblResultsDisplay;
         private System.Windows.Forms.Timer tmrGame;
         private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ProgressBar pgbNumber;
+        private System.Windows.Forms.ProgressBar pgbInput;
+        private System.Windows.Forms.ProgressBar pgbClear;
     }
 }
 
--- ICAs/CMPE1600BrandonFooteICA5/CMPE1600BrandonFooteICA5/Form1.cs
+++ ICAs/CMPE1600BrandonFooteICA5/CMPE1600BrandonFooteICA5/Form1.cs
@@ -11,9 +11,12 @@ namespace CMPE1600BrandonFooteICA5
 {
     public partial class Form1 : Form
     {
+        int waitCount = 0;
+        int playerInput = 0;
         string num = "";
         int number = 0;
         int count = 0;
+        int playerCounter = 0;
         Random generator = new Random();
         public Form1()
         {
@@ -32,17 +35,22 @@ namespace CMPE1600BrandonFooteICA5
 
         private void btnStartButton_Click(object sender, EventArgs e)
         {
+            pgbNumber.Value = 0;
+            pgbInput.Value = 0;
+            pgbClear.Value = 0;
+            waitCount = 0;
+            playerCounter = 0;
             number = 0;
             count = 0;
             num = "";
             tmrGame.Enabled = true;
             lblNumDisplay.Enabled = true;
+            tmrGame.Interval = 500;
         }
 
         private void tmrGame_Tick(object sender, EventArgs e)
         {
-
-            tmrGame.Interval = 500;
+            
             count++;
             lblNumDisplay.Text = generator.Next(0, 10).ToString();
             num += lblNumDisplay.Text;
@@ -51,12 +59,46 @@ namespace CMPE1600BrandonFooteICA5
 
             if (count <= 3)
             {
+                pgbNumber.Value++;
                 int.TryParse(num, out number);
             }
             else if (count >= 3)
             {
-                tmrGame.Enabled = false;
                 lblNumDisplay.Text = "";
+                btnStartButton.Enabled = false;
+                tbxUserInput.Enabled = true;
+                playerCounter++;
+                if (playerCounter <= 6)
+                {
+                    pgbInput.Value++;
+                    Console.WriteLine(playerCounter);
+                    tbxUserInput.Focus();
+                    int.TryParse(tbxUserInput.Text, out playerInput);
+                }
+                else if (playerCounter >= 6)
+                {
+                    pgbClear.Value++;
+                    if (playerInput == number)
+                    {
+                        lblResultsDisplay.Visible = true;
+                        lblResultsDisplay.Text = "Correct! Computer's Number: " + number + ", Your Number: " + playerInput;
+                    }
+                    else
+                    {
+                        lblResultsDisplay.Visible = true;
+                        lblResultsDisplay.Text = "Incorrect! Computer's Number: " + number + ", Your Number: " + playerInput;
+                    }
+                    waitCount++;
+                    if (waitCount == 10)
+                    {
+                        pgbClear.Value = 10;
+                        tmrGame.Enabled = false;
+                        btnStartButton.Enabled = true;
+                        lblResultsDisplay.Visible = false;
+                        tbxUserInput.Clear();
+                        tbxUserInput.Enabled = false;
+                    }
+                }
             }
         }
     }
