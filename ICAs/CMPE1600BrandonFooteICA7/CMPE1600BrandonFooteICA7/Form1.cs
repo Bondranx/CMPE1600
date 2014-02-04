83bd40d93cbc064f9e88f201aa428b67151571ae
Brandon Foote <brandon.foote@gmail.com>
2014-02-04 10:53:07 -0700

    ICA 7 First Commit
    ICA 7:
    Added Up arrow movement
    - Ball moves up one position when up arrow is struck
    - Ball loops to bottom if top wall is hit
    Added Down arrow movement
    - Ball moves down one position when down arrow is struck
    - Ball loops to top if bottom wall is hit
    Added Right arrow movement
    - Ball moves right one position when right arrow is struck
    - Ball loops to left side when right wall is hit
    Added Left arrow movement
    - Ball moves left one position when left arrow is struck
    - Ball loops to right side when left wall is struck

---
+++ ICAs/CMPE1600BrandonFooteICA7/CMPE1600BrandonFooteICA7/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace CMPE1600BrandonFooteICA7
+{
+    
+    public partial class Form1 : Form
+    {
+        public int xPos = 15;
+        public int yPos = 15;
+        CDrawer canvas = new CDrawer(600, 600);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            canvas.Scale = 20;
+            canvas.AddCenteredEllipse(xPos, yPos, 1, 1, Color.Red);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                yPos -=1;
+                if (yPos < 0)
+                    yPos = 30;
+                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, Color.Red);
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                yPos += 1;
+                if (yPos > 30)
+                    yPos = 0;
+                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, Color.Red);
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                xPos += 1;
+                if (xPos > 30)
+                    xPos = 0;
+                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, Color.Red);
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                xPos -= 1;
+                if (xPos < 0)
+                    xPos = 30;
+                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, Color.Red);
+            }
+        }
+    }
+}
