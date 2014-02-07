b8b9e8dbd452c36180551fbd3c829a2355939796
Brandon Foote <brandon.foote@gmail.com>
2014-02-07 08:35:19 -0700

    ICA 7 Commit Two
    - Changes circle color to Blue when 'B' key is pressed
    - Changes circle color to Green when 'G' key is pressed
    - Changes circle color to Red when 'R' key is pressed

--- ICAs/CMPE1600BrandonFooteICA7/CMPE1600BrandonFooteICA7/Form1.cs
+++ ICAs/CMPE1600BrandonFooteICA7/CMPE1600BrandonFooteICA7/Form1.cs
@@ -16,6 +16,9 @@ namespace CMPE1600BrandonFooteICA7
         public int xPos = 15;
         public int yPos = 15;
         CDrawer canvas = new CDrawer(600, 600);
+        public Color newColor = new Color();
+
+        
         public Form1()
         {
             InitializeComponent();
@@ -25,37 +28,52 @@ namespace CMPE1600BrandonFooteICA7
         {
             canvas.Scale = 20;
             canvas.AddCenteredEllipse(xPos, yPos, 1, 1, Color.Red);
+            newColor = Color.Red;
         }
-
+        
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+            
+            if (e.KeyCode == Keys.B)
+            {
+                newColor = Color.Blue;
+            }
+            else if (e.KeyCode == Keys.R)
+            {
+                newColor = Color.Red;
+            }
+            else if (e.KeyCode == Keys.G)
+            {
+                newColor = Color.Green;
+            }
+
             if (e.KeyCode == Keys.Up)
             {
                 yPos -=1;
                 if (yPos < 0)
                     yPos = 30;
-                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, Color.Red);
+                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, newColor);
             }
             else if (e.KeyCode == Keys.Down)
             {
                 yPos += 1;
                 if (yPos > 30)
                     yPos = 0;
-                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, Color.Red);
+                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, newColor);
             }
             else if (e.KeyCode == Keys.Right)
             {
                 xPos += 1;
                 if (xPos > 30)
                     xPos = 0;
-                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, Color.Red);
+                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, newColor);
             }
             else if (e.KeyCode == Keys.Left)
             {
                 xPos -= 1;
                 if (xPos < 0)
                     xPos = 30;
-                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, Color.Red);
+                canvas.AddCenteredEllipse(xPos, yPos, 1, 1, newColor);
             }
         }
     }
